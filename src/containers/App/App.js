ec3b97b4967c7f57ccc3b3e02c77c6a626ffa6c2
Jimmy Lin <usmcjimmy@gmail.com>
2016-12-17 16:39:28 -0500

    refactored main page content

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -115,17 +115,13 @@ export default class App extends Component {
           </Navbar.Collapse>
         </Navbar>
 
+        {/* App Content - starts */}
         <div className={styles.appContent}>
           {this.props.children}
         </div>
+        {/* App Content - ends */}
         <InfoBar/>
 
-        <div className="well text-center">
-          Have questions? Ask for help <a
-          href="https://github.com/erikras/react-redux-universal-hot-example/issues"
-          target="_blank">on Github</a> or in the <a
-          href="https://discord.gg/0ZcbPKXt5bZZb1Ko" target="_blank">#react-redux-universal</a> Discord channel.
-        </div>
       </div>
     );
   }
