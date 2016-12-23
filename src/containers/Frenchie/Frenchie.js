c4ad3facc133254afc68ea7a6f5c5f9a548d2ef5
Jimmy Lin <usmcjimmy@gmail.com>
2016-12-22 23:57:55 -0500

    added basic product rendering

--- src/containers/Frenchie/Frenchie.js
+++ src/containers/Frenchie/Frenchie.js
@@ -21,10 +21,52 @@ export default class Frenchie extends Component {
       products = <h3>No Proucts loaded</h3>;
     } else {
       products = productData.map((product) => {
-        const id = product.id;
-        const name = product.name;
+        const productDetails = {
+          Id: product.product_id,
+          Name: product.product_name,
+          Category: product.product_category,
+          Descriptions: product.product_descriptions,
+          Bullets: product.product_key_bullets,
+          Price: product.product_price,
+          Thumbnail: product.product_img_thumbnail,
+          Full_img: product.product_img_full,
+          ColorName: product.color_name,
+          Size: [product.xxsmall, product.xsmall, product.small, product.medium, product.large, product.xlarge, product.xxlarge]
+        };
+        let bullets;
+        const bulletsData = product.product_key_bullets;
+        bullets = bulletsData.map((bullet) => {
+          console.log('bullet', bullet);
+          bullets = <div><ul><li>{bullet}</li></ul></div>;
+        });
+        // const sizeData = [
+        //   {xxsmall: product.xxsmall},
+        //   {xsmal: product.xsmall},
+        //   {small: product.small},
+        //   {medium: product.medium},
+        //   {large: product.large},
+        //   {xlarge: product.xlarge},
+        //   {xxlarge: product.xxlarge},
+        // ];
         return (
-          <h4 key={id}>{name}</h4>
+          <h4 key={productDetails.Id}>
+            <img src={productDetails.Thumbnail} alt=""/>
+            <br/>
+            <br/>
+            Name: {productDetails.Name}
+            <br/>
+            Price: ${productDetails.Price}
+            <br/>
+            Category: {productDetails.Category}
+            <br/>
+            Descriptions: {productDetails.Descriptions}
+            <br/>
+            Bullets: {bullets}
+            <br/>
+            Colors: {productDetails.ColorName}
+            <br/>
+            Size: {productDetails.Size}
+          </h4>
         );
       });
     }
