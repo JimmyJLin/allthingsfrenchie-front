b5fb37eb2d7fe4e092b0e640913e3585c89cb167
Jimmy Lin <usmcjimmy@gmail.com>
2016-12-28 21:16:30 -0500

    changed css for friends, frenchie, footer

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -7,15 +7,13 @@ export default class Footer extends Component {
   render() {
     const styles = require('./Message.scss');
     return (
-      <div className="message">
-        <Grid className="container">
-          <div className={styles.message}>
-            <Row className="show-grid">
-              <Col xs={18} sm={16} md={12}>
-                <p data-content="FREE SHIPPING on all orders, no code required.  Excludes orders shipped outside of the contiguous U.S., P.O. Boxes, and APO/FPO addresses.">FREE SHIPPING on all Orders within the U.S.A!</p>
-              </Col>
-            </Row>
-          </div>
+      <div className={styles.message}>
+        <Grid className="container-fluid">
+          <Row className="show-grid">
+            <Col xs={18} sm={24} md={12}>
+              <p data-content="FREE SHIPPING on all orders, no code required.  Excludes orders shipped outside of the contiguous U.S., P.O. Boxes, and APO/FPO addresses.">FREE SHIPPING on all Orders within the U.S.A!</p>
+            </Col>
+          </Row>
         </Grid>
       </div>
     );
